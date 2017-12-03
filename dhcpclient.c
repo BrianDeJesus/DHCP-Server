1efa440846015e16c5929247df81876e41cd9a34
Brian Dejesus <dejes018@empress.csusm.edu>
2017-12-02 21:11:32 -0800

    Big Changes

--- dhcpclient.c
+++ dhcpclient.c
@@ -1,83 +1,73 @@
-/*
-Brian DeJesus
-extra credit Client
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
+#include <unistd.h>
 #include <errno.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
+#include <netinet/in.h>
 
 #define MAXLINE 80
 
-void mac_addr(unsigned char MAC_str[13])
+int main (int argc, char *argv[])
 {
-    #define HWADDR_len 6
-    int s,i;
-    struct ifreq ifr;
-    s = socket(AF_INET, SOCK_DGRAM, 0);
-    strcpy(ifr.ifr_name, "eth0");
-    ioctl(s, SIOCGIFHWADDR, &ifr);
-    for (i=0; i<HWADDR_len; i++)
-        sprintf(&MAC_str[i*2],"%02X",((unsigned char*)ifr.ifr_hwaddr.sa_data)[i]);
-    MAC_str[12]='\0';
-}
 
-int main(int argc, char *argv[])
-{
-    int	socket_fd;
-    struct  sockaddr_in servaddr;
-    unsigned char mac[13];
+    struct in_addr localInterface;
+    struct sockaddr_in cliaddr;
+    char   str[80];
 
-    mac_addr(mac);
-    puts(mac);
+    char *ipaddr;
+    char *lfaddr;
 
-    char str[80]; //buffer string
+    int port;
+    int fd;
 
     if (argc != 3) {
     	printf("Usage: tcpclient <address> <port> \n");
     	exit(-1);
     }
 
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd < 0) {
-    	perror("socket error");
-    	exit(-1);
+    ipaddr = argv[1];
+    port = atoi(argv[2]);
+    lfaddr  = "127.0.0.1";
+
+
+    /* Create a datagram socket on which to send. */
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0)
+    {
+        perror("Opening datagram socket error");
+        exit(1);
     }
 
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr(argv[1]);
-    servaddr.sin_port = htons(atoi(argv[2]));
+    /* Initialize the group sockaddr structure  */
+    memset((char *) &cliaddr, 0, sizeof(cliaddr));
+
+    bzero(&cliaddr, sizeof(cliaddr));
+    cliaddr.sin_family = AF_INET;
+    cliaddr.sin_addr.s_addr = inet_addr(ipaddr);
+    cliaddr.sin_port = htons(port);
 
 
-    if (connect(socket_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
-    	perror("connect error");
-    	exit(-1);
+    localInterface.s_addr = inet_addr(lfaddr);
+    if (setsockopt(fd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0)
+    {
+        fprintf(stderr, "Error setting local interface %x (%s) \n",
+                errno, strerror(errno));
+        exit(1);
+
     }
 
     while (fgets(str, MAXLINE, stdin) != NULL) {
-
-      write(socket_fd, str, sizeof(str));
-      printf("Sent data to server\n");
-      memset(str, 0, MAXLINE);
-      if (read(socket_fd, str, MAXLINE) == 0) {
-          printf("ERROR: server terminated \n");
-         break;
-       }
-
-      printf("From the SERVER: %s\n", str);
-
+        if (sendto (fd, str, MAXLINE, 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr)) < 0) {
+            fprintf(stderr, "Error sending datagram message: %x (%s) \n",
+                    errno, strerror(errno));
+            exit( -1);
+        }
     }
 
-    close(socket_fd);
-
-    return EXIT_SUCCESS;
+    close(fd);
 
 }
--- dhcpserver.c
+++ dhcpserver.c
@@ -1,189 +1,110 @@
-/*
-Brian DeJesus
-extra credit Server
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 #include <errno.h>
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <time.h>
-#include <pthread.h>
-
-pthread_t tid[3];
-
-struct thread_args {
-  char msg[50];
-  char ack[50];
-  char ip_offer[30];
-  int socket;
-};
-
-#define MAXLINE	256
-
-void sig_handler(int signal);
-void read_and_reply(int socket_fd);
-int found_key(char *buffer);
-void *reply_ack(void *data);
-void *reply_ip(void *data);
-void *reply_msg(void *data);
-
-const int backlog = 4;
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 
 int main(int argc, char *argv[])
 {
 
-    int	    listenfd, connfd;
-    pid_t   childpid;
-    int     clilen;
-    struct  sockaddr_in cliaddr, servaddr;
+    struct sockaddr_in servaddr;
+    struct ip_mreq group;
 
-    if (argc != 3) {// check command line arguments
-    	printf("Usage: tcpserver <address> <port> \n");
-    	return EXIT_FAILURE;
+    int fd;
+    int buflen;
+
+    char *ipaddr;
+    char *ifaddr;
+    char buf[80];
+
+    int port;
+    int reuse = 1;
+
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <ipaddr> <port> \n", argv[0]);
+        exit (-1);
     }
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listenfd == -1) {
-    	perror("socket error");
-    	return EXIT_FAILURE;
+    ipaddr = argv[1];
+    port = atoi(argv[2]);
+    ifaddr  = "127.0.0.1";
+
+    /* Create a datagram socket on which to receive. */
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0)
+    {
+        fprintf(stderr, "Error opening datagram socket %x (%s) \n",
+                errno, strerror(errno));
+        exit( -1);
+
     }
 
-    bzero(&servaddr, sizeof(servaddr));
+    /* Enable SO_REUSEADDR to allow multiple instances of this
+       application to receive copies of the multicast datagrams. */
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0)
+    {
+        fprintf(stderr, "Error setting SO_REUSEADDR %x (%s) \n",
+                errno, strerror(errno));
+        close(fd);
 
-    servaddr.sin_family        = AF_INET;
-    servaddr.sin_addr.s_addr   = inet_addr(argv[1]);
-    servaddr.sin_port          = htons(atoi(argv[2]));
+        exit( -1);
 
-    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
-	     perror("bind error");
-        return EXIT_FAILURE;
     }
 
-    if (listen(listenfd, backlog) == -1) {
-    	perror("listen error");
-    	return EXIT_FAILURE;
+    /* Bind to the proper port number with the IP address
+       specified as INADDR_ANY. */
+
+    memset((char *) &servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(fd, (struct sockaddr*)&servaddr, sizeof(servaddr)))
+    {
+        fprintf(stderr, "Error binding datagram socket %x (%s) \n",
+                errno, strerror(errno));
+        close(fd);
+        exit(-1);
+
     }
 
-    signal(SIGCHLD, sig_handler);
+    /* Join the multicast group  on the local host
+       interface. Note that this IP_ADD_MEMBERSHIP option must be
+       called for each local interface over which the multicast
+       datagrams are to be received. */
 
-    while (1) {
-      	clilen = sizeof(cliaddr);
-      	if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen)) < 0 ) {
-      		if (errno == EINTR)
-      			continue;
-      		else {
-      			perror("aceppt error");
-      			return EXIT_FAILURE;
-            close(connfd);
-      		}
-      	}
-        read_and_reply(connfd);
+    group.imr_multiaddr.s_addr = inet_addr(ipaddr);
+    group.imr_interface.s_addr = inet_addr(ifaddr);
+    if (setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&group, sizeof(group)) < 0)
+    {
+        fprintf(stderr, "Error adding multicast group %x (%s) \n",
+                errno, strerror(errno));
+        close(fd);
+        exit(1);
     }
 
-    return 0;
-}
+    /* Read from the socket. */
+    while(1) {
+      buflen = sizeof(buf);
+      if (read(fd, buf, buflen) < 0)
+      {
+          fprintf(stderr, "Error reading datagram message %x (%s) \n",
+                  errno, strerror(errno));
+          close(fd);
+          exit(1);
 
-void read_and_reply(int socket_fd) {
-	int	n, err;
-	char	buf[80];
-  struct thread_args *args = malloc(sizeof(struct thread_args));
-  strcpy(args->msg, "DHCP offer! \n");
-  strcpy(args->ack, "Server got the message! \n");
-  strcpy(args->ip_offer, "192.168.1.123");
-  args->socket = socket_fd;
-
-  err = pthread_create(&(tid[0]), NULL, &reply_ack, (void *)args);
- if (err != 0)
-     printf("\ncan't create thread 1 :[%s]", strerror(err));
-
- err = pthread_create(&(tid[1]), NULL, &reply_msg, (void *)args);
- if (err != 0)
-     printf("\ncan't create thread 2 :[%s]", strerror(err));
-
-err = pthread_create(&(tid[2]), NULL, &reply_ip, (void *)args);
-    if (err != 0)
-      printf("\ncan't create thread 2 :[%s]", strerror(err));
-
-
-	while (1) {
-      //   err = pthread_create(&(tid[0]), NULL, &reply_ack, (void *)args);
-      //  if (err != 0)
-      //      printf("\ncan't create thread 1 :[%s]", strerror(err));
-      //
-      //  err = pthread_create(&(tid[1]), NULL, &reply_msg, (void *)args);
-      //  if (err != 0)
-      //      printf("\ncan't create thread 2 :[%s]", strerror(err));
-      //
-      // err = pthread_create(&(tid[2]), NULL, &reply_ip, (void *)args);
-      //     if (err != 0)
-      //       printf("\ncan't create thread 2 :[%s]", strerror(err));
-
-      bzero(&buf, sizeof(buf));
-	    if ((n = read(socket_fd, buf, MAXLINE)) == 0) {
-        perror("error reading from client to server");
-  		    return;
-        }
-
-      if(found_key(buf) == 1) {
-        printf("discover FOUND\n");
-        pthread_join(tid[0], NULL);// offer message
-        pthread_join(tid[1], NULL);// off
-        pthread_join(tid[2], NULL);
       }
-    //  write(socket_fd, ack, strlen(ack));
-      printf("Message from client: %s \n" , buf);
 
-
-     //pthread_join(tid[0], NULL);// offer message
-
-	 }
-}
-
-void *reply_ack(void *data) { // Offer message function
-  struct thread_args *args = data;
-  write(args->socket, args->ack, strlen(args->ack));
-  return NULL;
-}
-
-void *reply_ip(void *data) { // Offer message function
-  struct thread_args *args = data;
-  write(args->socket, args->ip_offer, strlen(args->ip_offer));
-  return NULL;
-}
-
-void *reply_msg(void *data) { // Offer message function
-  struct thread_args *args = data;
-  write(args->socket, args->msg, strlen(args->msg));
-  return NULL;
-}
-
-
-int found_key(char *buffer) { // Check for DHCP discover
-  int i;
-  char key[] = "DHCP discover";
-  if(strncmp(buffer, key, strlen(key)) == 0){
-      return 1;
+      printf("The message from multicast server is: %s\n", buf);
     }
+
     return 0;
-}
-
-void sig_handler(int signal)
-{
-    pid_t    pid;
-    int	     status;
-
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
-	   printf("child %d terminated \n", pid);
-
-    return;
 
 }
